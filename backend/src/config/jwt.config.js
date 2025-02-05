e92113620b50d2dd3d43ca2dc80caa035da44afb
malek4522 <abdelmalek.2004@outlook.com>
2025-02-05 02:27:45 +0000

    test

--- backend/src/config/jwt.config.js
+++ backend/src/config/jwt.config.js
@@ -17,8 +17,9 @@ if (!process.env.JWT_SECRET || !process.env.JWT_REFRESH_SECRET) {
     },
     cookie: {
       httpOnly: true,
-      secure: true,
-      sameSite: 'none',
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     }
   }; 
